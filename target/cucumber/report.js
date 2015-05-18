27b147d94fb0171e56d93b71d5641d13648c762b
pedro henrique nazario da silva <pedrro2@hotmail.com>
2015-05-17 21:17:36 -0300

    Adicionando primeiro arquivo de feature, not working yet

---
+++ target/cucumber/report.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Portal_do_Aluno.feature");
+formatter.feature({
+  "id": "entrar-no-portal-do-aluno",
+  "description": "Eu como aluno\r\nDesejo entrar no portal do aluno\r\n\r\nCen�rio: Encontrar o portal do aluno\r\nDado que estou no site do Senac-RS\r\nQuando clico na aba Graduacao\r\nEnt�o o link para o portal do aluno aparece na tela",
+  "name": "Entrar no portal do Aluno",
+  "keyword": "Funcionalidade",
+  "line": 3,
+  "comments": [
+    {
+      "value": "# language: pt",
+      "line": 1
+    }
+  ]
+});
+});
---
+++ target/test-classes/Portal_do_Aluno.feature
@@ -0,0 +1,12 @@
+ # language: pt
+
+Funcionalidade: Entrar no portal do Aluno
+	Eu como aluno
+	Desejo entrar no portal do aluno
+	
+Contexto: Entrar no portal
+	
+Cenário: Encontrar o portal do aluno
+	Dado que estou no site do Senac-RS
+	Quando clico na aba Graduacao
+	Então o link para o portal do aluno aparece na tela
